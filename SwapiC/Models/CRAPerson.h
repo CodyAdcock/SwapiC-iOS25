92a743be9d0899c5dc84891256495565c69c5add
Cody <codyadcock10@gmail.com>
2019-03-28 17:48:49 -0600

    Second Commit, starting point

---
+++ SwapiC/Models/CRAPerson.h
@@ -0,0 +1,17 @@
+//
+//  CRAPerson.h
+//  SwapiC
+//
+//  Created by Cody on 3/28/19.
+//  Copyright © 2019 Cody Adcock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CRAPerson : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SwapiC/Models/CRAPerson.m
@@ -0,0 +1,13 @@
+//
+//  CRAPerson.m
+//  SwapiC
+//
+//  Created by Cody on 3/28/19.
+//  Copyright © 2019 Cody Adcock. All rights reserved.
+//
+
+#import "CRAPerson.h"
+
+@implementation CRAPerson
+
+@end
